46d79fd926045adf288163f5c7591f9498c2e4bf
fahadshaikh123487@gmail.com <fahadshaikh123487@gmail.com>
2020-01-29 11:52:19 -0800

    update

--- HomeTech/Controllers/HomeController.cs
+++ HomeTech/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
 
 namespace TechVibes.Controllers
 {
@@ -19,6 +21,29 @@ namespace TechVibes.Controllers
             string rtnmsg = "hello";
 
             return rtnmsg;
+        }
+        public static void Email()
+        {
+            try
+            {
+                
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress("FromMailAddress");
+                message.To.Add(new MailAddress("fahadshaikh123487@gmail.com"));
+                message.Subject = "Test";
+                message.IsBodyHtml = true; //to make message body as html 
+                string htmlString = "hello";
+                message.Body = htmlString;
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com"; //for gmail host  
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("FromMailAddress", "password");
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+            }
+            catch (Exception) { }
         }
     }
 
